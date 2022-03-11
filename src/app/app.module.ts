ab8322d68d5f1310e109593efc6174fcbac2c523
Fernando Pinto <fschultepinto@gmail.com>
2022-03-11 14:52:29 -0300

    Primeiro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
+import { FormLeadComponent } from './form-lead/form-lead.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    FormLeadComponent,
+      HeaderComponent
+   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    FormLeadComponent,
+  ]
 })
 export class AppModule { }
---
+++ src/app/form-lead/form-lead.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'app-form-lead',
+  templateUrl: './form-lead.component.html',
+  styleUrls: ['./form-lead.component.scss']
+})
+
+export class FormLeadComponent {
+
+}
