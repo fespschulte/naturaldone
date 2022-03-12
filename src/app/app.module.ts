20d6757ad842922082abe7f69cc934c4927e5d12
Fernando Pinto <fschultepinto@gmail.com>
2022-03-12 11:35:47 -0300

    12-03

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
     FormLeadComponent,
-      HeaderComponent
+      HeaderComponent,
+      FooterComponent
    ],
   imports: [
     BrowserModule
